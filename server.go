2322d33c2710e77f0d00a45e5b273a0c34e6c73d
rolandjitsu <rolandjitsu@gmail.com>
2015-05-24 00:15:42 +0200

    refactor(architecture): change app architecture

--- server.go
+++ server.go
@@ -3,17 +3,21 @@ package main
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/gzip"
+	"bytes"
 	"log"
 	"net/http"
 	"path"
 )
+
+const STATIC_FILES_PATH string = "./"
+const APP_PATH string = "./app"
 
 var m *martini.Martini
 
 func init() {
 	m = martini.New()
 	router := martini.NewRouter()
-	static := martini.Static("./src", martini.StaticOptions{ Fallback: "./src/index.html" })
+	static := martini.Static(STATIC_FILES_PATH)
 
 	m.Use(martini.Recovery())
 	m.Use(martini.Logger())
@@ -22,7 +26,7 @@ func init() {
 
 	router.NotFound(func(res http.ResponseWriter, req *http.Request) {
 		if path.Ext(req.URL.Path) == "" {
-			http.ServeFile(res, req, "./src/index.html")
+			http.ServeFile(res, req, ConcatStrings(APP_PATH, "/index.html"))
 		} else {
 			res.WriteHeader(http.StatusNotFound)
 		}
@@ -35,4 +39,16 @@ func main() {
 	if err := http.ListenAndServe(":8000", m); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func ConcatStrings(strings ...string) string {
+	var buffer bytes.Buffer
+	length := len(strings)
+	if length == 0 {
+		return ""
+	}
+	for i := 0; i < length; i++ {
+		buffer.WriteString(strings[i])
+	}
+	return buffer.String()
 }
