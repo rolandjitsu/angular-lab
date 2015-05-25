da96aa3b80e6ca5983adc02f5c847e9ec417d1fd
rolandjitsu <rolandjitsu@gmail.com>
2015-05-25 19:47:37 +0200

    feat(shadow DOM): check if shadow DOM is supported and add as injectable on app bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,24 @@
+import { bind } from 'angular2/di';
 import { bootstrap } from 'angular2/core';
+import { ShadowDomStrategy, NativeShadowDomStrategy } from 'angular2/core';
+import { StyleUrlResolver } from 'angular2/src/render/dom/shadow_dom/style_url_resolver';
 import { routerInjectables } from 'angular2/router';
-import { App } from 'app';
+import { App } from 'components/app';
+
+let hasShadowDom = Boolean(
+	document && document.body && document.body.createShadowRoot
+);
+
+let shadowDomInjectables = [
+	!hasShadowDom
+		? []
+		: bind(ShadowDomStrategy).toFactory(
+			styleUrlResolver => new NativeShadowDomStrategy(styleUrlResolver),
+			[StyleUrlResolver]
+		)
+]
 
 bootstrap(App, [
+	shadowDomInjectables,
 	routerInjectables
 ]);
