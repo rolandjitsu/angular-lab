b01ac6288047f6ea2ebd54b6a1a69d4a27c37497
rolandjitsu <rolandjitsu@gmail.com>
2015-06-12 08:49:48 +0200

    feat(raf): implement a wrapper around requestAnimationFrame and an animation end observer

---
+++ src/app/services/animation.ts
@@ -0,0 +1,58 @@
+import { isFunction } from 'angular2/src/facade/lang';
+
+let time: Function;
+let ns: number;
+let rAF = window['requestAnimationFrame'] || window['webkitRequestAnimationFrame'] || window['mozRequestAnimationFrame'];
+let cAF = window['cancelAnimationFrame'] || window['webkitCancelAnimationFrame'] || window['webkitCancelRequestAnimationFrame'] || window['mozCancelRequestAnimationFrame'];
+
+function getAnimationEndEventName (): string {
+	let fakeEl = document.createElement('fakeelement');
+	let animationEndEventNames = {
+		'animation': 'animationend',
+		'-o-animation': 'oAnimationEnd',
+		'-moz-animation': 'animationend',
+		'-webkit-animation': 'webkitAnimationEnd'
+	};
+	for (let eventName in animationEndEventNames) {
+		if (typeof fakeEl.style[eventName] !== 'undefined') return animationEndEventNames[eventName];
+	}
+	return null;
+}
+
+if (!performance || isFunction(performance.now)) {
+	ns = performance.timing.navigationStart;
+	time = function (): number {
+		return performance.now() + ns;
+	}
+}
+else {
+	time = Date.now
+}
+
+export class AnimationEndObserver {
+	element: HTMLElement;
+	event: string = getAnimationEndEventName();
+	handler: EventListener;
+	constructor(element: HTMLElement, handler: EventListener) {
+		this.element = element;
+		this.handler = handler;
+		this.element.addEventListener(this.event, this.handler, false);
+	}
+	static subscribe(element: HTMLElement, done: Function, context?: any): AnimationEndObserver {
+		return new AnimationEndObserver(element, event => {
+			done.apply(context, [event]);
+		});
+	}
+	disconnect() {
+		this.element.removeEventListener(this.event, this.handler);
+	}
+}
+
+export class Animation {
+	static rAF(fn: Function, context?: any): number {
+		return rAF(timestamp => fn.apply(context, [timestamp + ns]));
+	}
+	static cAF (id: number): void {
+		cAF(id);
+	}
+}
