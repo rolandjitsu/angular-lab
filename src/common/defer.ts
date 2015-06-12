013d0fa9c5e25907ad28a12ec2076caf5a0d4477
rolandjitsu <rolandjitsu@gmail.com>
2015-06-12 08:38:21 +0200

    feat(defer): implement a simple defer like class

---
+++ src/common/defer.ts
@@ -0,0 +1,12 @@
+export class Defer<R> {
+	promise: Promise<R>;
+	resolve: (value?: R | Thenable<R>) => void;
+	reject: (error?: any) => void;
+	constructor() {
+		let that: Defer<R> = this;
+		this.promise = new Promise<R>((resolve, reject) => {
+			that.resolve = resolve;
+	        that.reject = reject;
+		});
+	}
+}
