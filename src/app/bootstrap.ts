e0e6e3ee3fa6985ff7263c085d71d6e383bf6d70
rolandjitsu <rolandjitsu@gmail.com>
2015-06-12 08:50:54 +0200

    chore(jit): remove jit injectable

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,19 +1,17 @@
 import { bootstrap } from 'angular2/angular2';
 import { routerInjectables } from 'angular2/router';
 
-import { formInjectables } from 'common/form';
 import { shadowDomInjectables } from 'common/shadow_dom'; // custom injectable that checks if ShadowDom is available to inject
-import { jitInjectables } from 'common/jit'; // custom injectable that uses Just-In-Time change detection
+import { formInjectables } from 'common/form';
 import { pipeInjectables } from 'app/pipes';
 import { serviceInjectables } from 'app/services';
 
 import { App } from 'app/components';
 
 bootstrap(App, [
-	routerInjectables,
-	formInjectables,
 	shadowDomInjectables,
-	// jitInjectables,
+	formInjectables,
+	routerInjectables,
 	pipeInjectables,
 	serviceInjectables
 ]);
--- src/common/jit.ts
+++
@@ -1,6 +0,0 @@
-import { ChangeDetection, JitChangeDetection } from 'angular2/angular2';
-import { bind } from 'angular2/angular2';
-
-export var jitInjectables: Array<any> = [
-	bind(ChangeDetection).toClass(JitChangeDetection)
-];
