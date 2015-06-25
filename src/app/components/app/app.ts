eed4ee1bd54f8dc586c7a94f407051adb858425a
rolandjitsu <rolandjitsu@gmail.com>
2015-06-25 12:37:41 +0200

    fix(app): fix routing

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -1,4 +1,3 @@
-import { BrowserLocation } from 'angular2/src/router/browser_location';
 import { Component, View } from 'angular2/annotations';
 import { ViewContainerRef, ElementRef } from 'angular2/core';
 import { Router, RouteConfig, RouterOutlet, RouterLink } from 'angular2/router';
@@ -8,8 +7,9 @@ import { Logo } from 'app/directives';
 import { Todos } from '../todos/todos';
 
 interface IRoute<T> {
-	component: T,
 	path: string,
+	component?: T,
+	redirectTo?: string,
 	as?: string
 }
 
@@ -20,7 +20,7 @@ interface IRoutes {
 let routes: IRoutes = [
 	{
 		component: Todos,
-		path: '/routefix', // temp fix until router is fixed, when fixed remove from router.navigate below
+		path: '/',
 		as: 'todos'
 	}
 ]
@@ -45,20 +45,20 @@ let routes: IRoutes = [
 
 export class App {
 	loading: boolean = true;
-	constructor(private viewContainer: ViewContainerRef, private elementRef: ElementRef, router: Router, browserLocation: BrowserLocation) {
+	constructor(private viewContainer: ViewContainerRef, private elementRef: ElementRef, router: Router) {
 		let that: App = this;
-		let uri: string = browserLocation.path();
 		let root: Firebase = new Firebase("https://ng2-play.firebaseio.com");
 		let auth: FirebaseAuthData = root.getAuth();
-		router
-			.navigate('/routefix') // temp fix until router is fixed
-			// .navigate(auth === null ? '/routefix' : uri) // we need to manually go to the correct uri until the router is fixed
-			.then((_) => setTimeout(
-				(_) => that.loading = false,
-				3000
-			));
+
 		root.onAuth(auth => {
 			if (auth === null) root.authAnonymously(() => {});
+		});
+		router.subscribe((path) => {
+			if (!that.loading) return;
+			setTimeout(
+				(_) => that.loading = false,
+				3000
+			);
 		});
 		
 		
@@ -66,7 +66,7 @@ export class App {
 		 * Animations
 		 */
 		
-		let el: HTMLElement = this.elementRef.domElement;
+		let el: HTMLElement = this.elementRef.nativeElement;
 		let main: HTMLElement = <HTMLElement>el.querySelector('* /deep/ .js-main');
 		let logo: HTMLElement = <HTMLElement>el.querySelector('* /deep/ .js-logo');
 		let mainSub = AnimationEndObserver.subscribe(
