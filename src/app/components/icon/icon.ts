776ed55d3d9573ce3dbc54252a40cc939035510d
rolandjitsu <rolandjitsu@gmail.com>
2015-06-29 11:06:04 +0200

    fix(icon): add icon component to git

---
+++ src/app/components/icon/icon.ts
@@ -0,0 +1,35 @@
+import { Component, View } from 'angular2/annotations';
+import { ViewContainerRef } from 'angular2/core';
+
+import { supportsNativeShadowDOM } from 'common/shadow_dom';
+import { IconStore } from 'app/services';
+
+@Component({
+	selector: 'icon',
+	properties: [
+		'src'
+	]
+})
+
+@View({
+	templateUrl: 'app/components/icon/icon.html', // remove once the relative resolve of styles will work properly
+	styleUrls: [
+		'app/components/icon/icon.css'
+	]
+})
+
+export class Icon {
+	el;
+	constructor(private viewContainer: ViewContainerRef, public icon: IconStore) {
+		this.el = this.viewContainer.element.nativeElement;
+	}
+	set src(src: string) {
+		let that: Icon = this;
+		let root;
+		if (supportsNativeShadowDOM) root = this.el.shadowRoot;
+		else root = this.el; 
+		this.icon.get(src).then((svg) => {
+			root.appendChild(svg);
+		});
+	}
+}
