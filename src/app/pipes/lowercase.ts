f4b32a820cdee18ab55181095bc209193659ee47
rolandjitsu <rolandjitsu@gmail.com>
2015-09-07 08:17:08 +0200

    chore(lowercase pipe): cleanup some checks

--- src/app/pipes/lowercase.ts
+++ src/app/pipes/lowercase.ts
@@ -1,15 +1,12 @@
 import { Injectable, Pipe, PipeTransform } from 'angular2/angular2';
 
-import { isBlank, isString } from 'common/lang';
+import { isBlank } from 'common/lang';
 
 @Pipe({ name: 'lowercase' })
 @Injectable()
 export class LowerCasePipe implements PipeTransform {
 	transform(value: string, args: List<any> = null): string {
 		if (isBlank(value)) return value;
-		if (!isString(value)) {
-			// throw new InvalidPipeArgumentException(LowerCasePipe, value);
-		}
 		return value.toLowerCase();
 	}
 }
