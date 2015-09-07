f90db474b8c07ae0cb1edfc808fd6f337efe0451
rolandjitsu <rolandjitsu@gmail.com>
2015-09-07 18:24:43 +0200

    chore(travis): run CI on all branches, but skip deployments if not master branch

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
 	echo -e "${FIREBASE_EMAIL}\n${FIREBASE_PASSWORD}" | firebase deploy;
 fi
