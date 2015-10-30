073dad13df84a4040270edd7e0c70016169f520f
rolandjitsu <rolandjitsu@gmail.com>
2015-10-30 18:08:57 +0100

    fix(travis): change FIREBASE_TOKEN and check it

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo ${FIREBASE_TOKEN}
+
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
 	./node_modules/.bin/gulp deploy:hosting/ci
 fi
