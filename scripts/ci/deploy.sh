f5a815feef831e5fe3e5e3a0d7b3f693532390b7
rolandjitsu <rolandjitsu@gmail.com>
2015-10-30 19:18:36 +0100

    chore(scripts): clean echo statement

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo ${FIREBASE_TOKEN}
-
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
 	./node_modules/.bin/gulp deploy:hosting/ci
 fi
