682156e2b0ffbec4797d44e2557e14cfe671ab4f
rolandjitsu <rolandjitsu@gmail.com>
2015-11-19 17:25:21 +0100

    chore: cleanup checkbox component [ci skip]

--- src/app/components/checkbox/checkbox.ts
+++ src/app/components/checkbox/checkbox.ts
@@ -2,7 +2,6 @@ import {
 	Input,
 	Output,
 	HostBinding,
-	ElementRef,
 	HostListener,
 	EventEmitter,
 	Component,
@@ -10,24 +9,12 @@ import {
 	Attribute,
 	ViewEncapsulation,
 	ControlValueAccessor,
-	NgControl,
-	OpaqueToken,
-	forwardRef,
-	Provider
+	NgControl
 } from 'angular2/angular2';
 
 import { isPresent } from '../../../common/lang';
 import { KEY_CODES } from '../../../common/key_codes';
 import { Glyph } from '../glyph/glyph';
-
-const NG_VALUE_ACCESSOR: OpaqueToken = new OpaqueToken('NgValueAccessor');
-const CHECKBOX_VALUE_ACCESSOR = new Provider(
-	NG_VALUE_ACCESSOR,
-	{
-		useExisting: forwardRef(() => Checkbox),
-		multi: true
-	}
-);
 
 @Component({
 	selector: 'checkbox[ng-control], checkbox[ng-form-control], checkbox[ng-model]',
@@ -35,9 +22,6 @@ const CHECKBOX_VALUE_ACCESSOR = new Provider(
 	templateUrl: 'app/components/checkbox/checkbox.html',
 	styleUrls: [
 		'app/components/checkbox/checkbox.css'
-	],
-	providers: [
-		CHECKBOX_VALUE_ACCESSOR
 	],
 	directives: [
 		Glyph
@@ -49,25 +33,18 @@ const CHECKBOX_VALUE_ACCESSOR = new Provider(
 
 export class Checkbox implements ControlValueAccessor {
 	@Input('checked') _checked: boolean = false;
-	@Output() update: EventEmitter = new EventEmitter();
-
-	@HostListener('change', [
-		'$event'
-	])
-	onChange;
+	@Output('change') onChange: EventEmitter<any> = new EventEmitter();
 
 	@HostListener('blur', [
 		'$event'
 	])
 	onTouched;
 
-	elementRef: ElementRef;
 	private _tabindex: number;
 	private _disabled: boolean = false;
 
-	constructor(elementRef: ElementRef, @Self() cd: NgControl, @Attribute('tabindex') tabindex: string) {
+	constructor(@Self() cd: NgControl, @Attribute('tabindex') tabindex: string) {
 		this._tabindex = isPresent(tabindex) ? parseInt(tabindex, 10) : 0;
-		this.elementRef = elementRef;
 		cd.valueAccessor = this; // Validation will not work if we don't set the control's value accessor
 	}
 
@@ -82,7 +59,7 @@ export class Checkbox implements ControlValueAccessor {
 	}
 
 	@HostBinding('attr.aria-disabled')
-	get disabled() {
+	get disabled(): boolean {
 		return this._disabled;
 	}
 
@@ -95,20 +72,12 @@ export class Checkbox implements ControlValueAccessor {
 		this._disabled = isPresent(value) && value !== false;
 	}
 
-	toggle(event) {
+	toggle(event): void {
 		if (this.disabled) return event.stopPropagation();
-		this._checked = !this._checked;
-		// This will make sure that NgControl picks up on the change
-		// Custom elements don't have `change` events, so we emulate it
-		// https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
-		let ev = new Event('change');
-		this.elementRef.nativeElement.dispatchEvent(ev);
-		this.update.next(this._checked);
+		this.onChange.next(!this._checked);
 	}
 	registerOnChange(fn: (_: any) => void): void {
-		this.onChange = (...args) => {
-			fn.apply(null, args);
-		};
+		this.onChange.subscribe(fn);
 	}
 	registerOnTouched(fn: () => void): void {
 		this.onTouched = fn;
@@ -120,7 +89,7 @@ export class Checkbox implements ControlValueAccessor {
 	@HostListener('keydown', [
 		'$event'
 	])
-	onKeydown(event: KeyboardEvent) {
+	onKeydown(event: KeyboardEvent): void {
 		if (event.keyCode !== KEY_CODES.SPACE) return;
 		event.preventDefault();
 		this.toggle(event);
