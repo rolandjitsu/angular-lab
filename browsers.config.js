8b854cf646d524b4a592ea7ac0de5e61c8106b8e
rolandjitsu <rolandjitsu@gmail.com>
2017-02-16 14:24:33 +0200

    fix: fix Safari platform name

--- browsers.config.js
+++ browsers.config.js
@@ -81,7 +81,7 @@ const CUSTOM_LAUNCHERS = {
 	'SL_SAFARI10': {
 		base: 'SauceLabs',
 		browserName: 'safari',
-		platform: 'MacOS 10.12',
+		platform: 'macOS 10.12',
 		version: '10.0'
 	},
 	// IE
