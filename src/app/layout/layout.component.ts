9688f57975485045963df25b66fa613ef63444cf
rolandjitsu <rolandjitsu@gmail.com>
2017-02-17 22:13:59 +0200

    chore(LayoutComponent): cleanup when the view is destroyed

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,13 @@ import {
 	Component,
 	ViewChild,
 	OnInit,
+	OnDestroy,
 	Inject
 } from '@angular/core';
 import {Router} from '@angular/router';
 import {MdSidenav} from '@angular/material';
 import {ObservableMedia} from '@angular/flex-layout';
+import {Subscription} from 'rxjs/Subscription';
 
 
 @Component({
@@ -16,31 +18,39 @@ import {ObservableMedia} from '@angular/flex-layout';
 		'./layout.component.scss'
 	]
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
 	@ViewChild('sidenav') sidenav: MdSidenav;
 
 	mode = 'side';
 	showMenuButton = false;
 
+	private subs: Subscription[] = [];
+
 	constructor(private router: Router, @Inject(ObservableMedia) public media: any) {}
 
 	ngOnInit(): void {
 		// https://github.com/angular/flex-layout/wiki/Adaptive-Layouts
-		this.media.subscribe(() => {
-			const isGtSm = this.media.isActive('gt-sm');
-			this.showMenuButton = !isGtSm;
+		this.subs.push(...[
+			this.media.subscribe(() => {
+				const isGtSm = this.media.isActive('gt-sm');
+				this.showMenuButton = !isGtSm;
 
-			if (isGtSm) {
-				this.sidenav.open();
-				this.mode = 'side';
-			} else {
-
-				this.sidenav.close()
-					.then(() => {
-						this.mode = 'over';
-					});
-			}
-		});
+				if (isGtSm) {
+					this.sidenav.open();
+					this.mode = 'side';
+				} else {
+					this.sidenav.close()
+						.then(() => {
+							this.mode = 'over';
+						});
+				}
+			})
+		]);
+	}
+	ngOnDestroy(): void {
+		for (const sub of this.subs) {
+			sub.unsubscribe();
+		}
 	}
 
 	navigate(link: any[]): void {
