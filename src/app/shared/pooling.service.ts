5cd0fa709ac364794b4a4e02769e8bdd2e48bf84
rolandjitsu <rolandjitsu@gmail.com>
2017-02-17 22:35:39 +0200

    chore(PoolingService): revert to previous version

--- src/app/shared/pooling.service.ts
+++ src/app/shared/pooling.service.ts
@@ -1,8 +1,6 @@
-import {Injectable, NgZone} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
-import {Subject} from 'rxjs/Subject';
-import {Subscription} from 'rxjs/Subscription';
-import {Observer} from 'rxjs/Observer';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/multicast';
@@ -10,40 +8,11 @@ import 'rxjs/add/operator/multicast';
 
 @Injectable()
 export class PoolingService {
-	constructor(private zone: NgZone) {}
-
 	// NOTE: Running the interval outside Angular ensures that e2e tests will not hang.
 	execute<T>(operation: () => Observable<T>, frequency: number = 1000): Observable<T> {
-		const subject = new Subject();
-		const source = Observable.create((observer: Observer<T>) => {
-			let sub: Subscription;
-			this.zone.runOutsideAngular(() => {
-				const zone = this.zone;
-				sub = Observable.interval(frequency)
-					.mergeMap(operation)
-					.subscribe({
-						next(result) {
-							zone.run(() => {
-								observer.next(result);
-							});
-						},
-						error(err) {
-							zone.run(() => {
-								observer.error(err);
-							});
-						}
-					});
-			});
-
-			return () => {
-				if (sub) {
-					sub.unsubscribe();
-				}
-			};
-		});
-
-
-		return source.multicast(subject)
+		return Observable.interval(frequency)
+			.mergeMap(operation)
+			.multicast(() => new ReplaySubject())
 			.refCount();
 	}
 }
