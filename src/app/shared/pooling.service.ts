f642868e757767aec914c08532671bab979f7d43
rolandjitsu <rolandjitsu@gmail.com>
2017-02-18 17:56:41 +0200

    test(PoolingService): add tests for execute()

--- src/app/shared/pooling.service.ts
+++ src/app/shared/pooling.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {ReplaySubject} from 'rxjs/ReplaySubject';
+import {IScheduler} from 'rxjs/Scheduler';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/multicast';
@@ -8,10 +9,13 @@ import 'rxjs/add/operator/multicast';
 
 @Injectable()
 export class PoolingService {
-	// NOTE: Running the interval outside Angular ensures that e2e tests will not hang.
-	execute<T>(operation: () => Observable<T>, frequency: number = 1000): Observable<T> {
-		return Observable.interval(frequency)
-			.mergeMap(operation)
+	// NOTE: If we run unit tests, we need to use TestScheduler,
+	// therefore, we provide an optional arg for using a different scheduler.
+	execute<T>(operation: () => Observable<T>, frequency: number = 1000, scheduler?: IScheduler): Observable<T> {
+		const interval = scheduler
+			? Observable.interval(frequency, scheduler)
+			: Observable.interval(frequency);
+		return interval.mergeMap(operation)
 			.multicast(() => new ReplaySubject())
 			.refCount();
 	}
