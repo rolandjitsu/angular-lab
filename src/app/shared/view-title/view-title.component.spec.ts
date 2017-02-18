bb50154da2d3ebe86bb7c76feb1dc43cc35e53be
rolandjitsu <rolandjitsu@gmail.com>
2017-02-18 18:57:29 +0200

    test(ViewTitleComponent): unit test behaviour

---
+++ src/app/shared/view-title/view-title.component.spec.ts
@@ -0,0 +1,31 @@
+import {TestBed, async, inject} from '@angular/core/testing';
+import {ViewTitleComponent} from './view-title.component';
+import {AppModule} from '../../app.module';
+import {ViewTitleService} from '../../core';
+
+
+describe('ViewTitleComponent', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({imports: [AppModule]});
+		TestBed.compileComponents();
+	});
+
+	it('should create the <rj-view-title> component', async(() => {
+		const fixture = TestBed.createComponent<ViewTitleComponent>(ViewTitleComponent);
+		const viewTitleComponent: ViewTitleComponent = fixture.debugElement.componentInstance;
+		fixture.detectChanges();
+		expect(viewTitleComponent)
+			.toBeTruthy();
+		fixture.destroy();
+	}));
+
+	it('should update the text when ViewTitleService emits a new value', async(inject([ViewTitleService], (viewTitle: ViewTitleService) => {
+		const fixture = TestBed.createComponent<ViewTitleComponent>(ViewTitleComponent);
+		const viewTitleComponent: ViewTitleComponent = fixture.debugElement.componentInstance;
+		fixture.detectChanges();
+		viewTitle.set('Test');
+		fixture.detectChanges();
+		expect(viewTitleComponent.title).toEqual('Test');
+		fixture.destroy();
+	})));
+});
