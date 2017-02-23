fb4cac5f42b89c6ce45b5492e526ca2bb9ec0848
rolandjitsu <rolandjitsu@gmail.com>
2017-02-23 20:46:05 +0200

    chore: don't show progress when making deployments on the CI

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -4,5 +4,5 @@ set -e
 # Only make deployments from the master,
 # PRs will not be deployed.
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-	$(npm bin)/firebase deploy --token ${FIREBASE_TOKEN}
+	$(npm bin)/firebase deploy --token ${FIREBASE_TOKEN} --non-interactive
 fi
