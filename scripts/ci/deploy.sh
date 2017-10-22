eef2da812001a0d71600e848866dcfe11426d02e
rolandjitsu <rolandjitsu@gmail.com>
2017-10-22 17:35:54 +0200

    chore: fix failing travis build

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -4,8 +4,6 @@ set -e
 # Only make deployments from the master,
 # PRs will not be deployed.
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-    # Generate the .secrets.js file (frm which our env files read)
-    npm run secrets:eject
     # Deploy app
     $(npm bin)/firebase deploy -P production \
         --token ${FIREBASE_TOKEN} \
--- src/environments/firebase.ts
+++ src/environments/firebase.ts
@@ -2,9 +2,9 @@ import {FIREBASE_API_KEY} from 'secrets';
 
 export default {
     apiKey: FIREBASE_API_KEY,
-    authDomain: "angular-laboratory.firebaseapp.com",
-    databaseURL: "https://angular-laboratory.firebaseio.com",
-    projectId: "angular-laboratory",
-    storageBucket: "angular-laboratory.appspot.com",
-    messagingSenderId: "386897844771"
+    authDomain: 'angular-laboratory.firebaseapp.com',
+    databaseURL: 'https://angular-laboratory.firebaseio.com',
+    projectId: 'angular-laboratory',
+    storageBucket: 'angular-laboratory.appspot.com',
+    messagingSenderId: '386897844771'
 };
