fe2b3f594ba9d4befd8ed6e5794283f24820dbab
rolandjitsu <rolandjitsu@gmail.com>
2017-10-22 14:25:40 +0200

    chore: specify project to deploy to

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -4,5 +4,7 @@ set -e
 # Only make deployments from the master,
 # PRs will not be deployed.
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]] && [[ "${TRAVIS_BRANCH}" == "master" ]]; then
-    $(npm bin)/firebase deploy --token ${FIREBASE_TOKEN} --non-interactive
+    $(npm bin)/firebase deploy -P production \
+        --token ${FIREBASE_TOKEN} \
+        --non-interactive
 fi
